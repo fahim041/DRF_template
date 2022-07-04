ba60b2645d7ee1c76c6f94c9e3c25a394d1abe37
Fahim090 <46305384+M0nster92@users.noreply.github.com>
2022-07-04 15:15:55 -0400

    file directory for s3

--- s3_file_upload/uploader/models.py
+++ s3_file_upload/uploader/models.py
@@ -5,6 +5,6 @@ from django.db import models
 
 class Uploader(models.Model):
     title = models.CharField(max_length=30)
-    document = models.FileField(max_length=30)
+    document = models.FileField(max_length=500, upload_to="pic/")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
